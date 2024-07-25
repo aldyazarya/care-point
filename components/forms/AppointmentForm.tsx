2302e74dbb8278aa8a231d4f007d20dbda6ee665
aldy azarya <azarya.dev@gmail.com>
2024-07-25 16:02:27 +0700

    create admin dashboard

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -17,7 +17,10 @@ import { createUser } from "@/lib/actions/patient.actions";
 import { Doctors } from "@/constans";
 import { SelectItem } from "../ui/select";
 import Image from "next/image";
-import { createAppointment } from "@/lib/actions/appointment.actions";
+import {
+  createAppointment,
+  updateAppointment,
+} from "@/lib/actions/appointment.actions";
 import { Appointment } from "@/types/appwrite.types";
 
 const AppointmentForm = ({
@@ -25,11 +28,13 @@ const AppointmentForm = ({
   patientId,
   type = "create",
   appointment,
+  setOpen,
 }: {
   userId: string;
   patientId: string;
-  type: "create" | "cancel" | "schedule";
+  type: "create" | "cancel" | "scheduled";
   appointment?: Appointment;
+  setOpen: (open: boolean) => void;
 }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
@@ -54,11 +59,11 @@ const AppointmentForm = ({
 
     let status;
     switch (type) {
-      case "schedule":
+      case "scheduled":
         status = "scheduled";
         break;
       case "cancel":
-        status = "canceled";
+        status = "cancelled";
         break;
       default:
         status = "pending";
@@ -76,12 +81,32 @@ const AppointmentForm = ({
           note: values.note,
           status: status as Status,
         };
+
         const appointment = await createAppointment(appointmentData);
         if (appointment) {
           form.reset();
           router.push(
             `/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`
           );
+        }
+      } else {
+        const appointmentToUpdate = {
+          userId,
+          appointmentId: appointment?.$id!,
+          appointment: {
+            primaryPhysician: values?.primaryPhysician,
+            schedule: new Date(values?.schedule),
+            status: status as Status,
+            cancellationReason: values?.cancellationReason,
+          },
+          type,
+        };
+
+        const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+        if (updatedAppointment) {
+          setOpen && setOpen(false);
+          form.reset();
         }
       }
     } catch (error) {
@@ -100,7 +125,7 @@ const AppointmentForm = ({
     case "create":
       buttonLabel = "Create Appointment";
       break;
-    case "schedule":
+    case "scheduled":
       buttonLabel = "Schedule Appointment";
       break;
     default:
@@ -110,12 +135,14 @@ const AppointmentForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-          <h1 className="header">New Appointmnet </h1>
-          <p className=" text-dark-700">
-            Request a new appointment in 10 seconds.
-          </p>
-        </section>
+        {type === "create" && (
+          <section className="mb-12 space-y-4">
+            <h1 className="header">New Appointmnet </h1>
+            <p className=" text-dark-700">
+              Request a new appointment in 10 seconds.
+            </p>
+          </section>
+        )}
 
         {type !== "cancel" && (
           <>
