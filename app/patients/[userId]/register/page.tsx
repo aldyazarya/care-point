1ee221aa65e9cc48d038014255f8d709eaa8d942
aldy azarya <azarya.dev@gmail.com>
2024-07-25 18:53:47 +0700

    setup Sentry for monitoring website

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -4,8 +4,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import * as Sentry from "@sentry/nextjs";
+
 const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
+
+  Sentry.metrics.set("user_view_register", user.name);
 
   return (
     <div className=" flex h-screen max-h-screen">
@@ -21,9 +25,7 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
 
           <RegisterForm user={user} />
 
-          <p className=" copyright py-12">
-            © 2024 CarePoint
-          </p>
+          <p className=" copyright py-12">© 2024 CarePoint</p>
         </div>
       </section>
 
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://bbc783d00f4c10272c96d2b0835463e6@o4507662193524736.ingest.us.sentry.io/4507662196867072",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
